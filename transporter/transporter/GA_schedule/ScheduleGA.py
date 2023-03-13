1ccd9290ddad42039318be8c5e166b343c77756d
fly-like-you <58936850+fly-like-you@users.noreply.github.com>
2023-03-13 13:38:27 +0900

    그래프 그리는 코드 + 그래프 데이터 csv 파일추가

--- transporter/transporter/GA_schedule/ScheduleGA.py
+++ transporter/transporter/GA_schedule/ScheduleGA.py
@@ -1,4 +1,3 @@
-import math
 import random
 
 import numpy as np
@@ -10,7 +9,7 @@ from transporter.transporter.create_data.Block import Block
 
 
 class ScheduleGA:
-    def __init__(self, blocks, population_size=10, max_generation=10, elitism_rate=0.4, mutation_rate=0.1):
+    def __init__(self, blocks, population_size=10, max_generation=100, elitism_rate=0.3, mutation_rate=0.2):
         self.blocks = blocks
         self.population_size = population_size
         self.max_generation = max_generation
@@ -27,16 +26,22 @@ class ScheduleGA:
 
     def run(self):
         mutation = Mutation(self.mutation_rate)
+        prev_result = 0
+
         for generation in range(self.max_generation):
             selection = Selection(self.population, self.block_dict)
             crossover = Crossover(selection, 1)
             # 적합도 평가
             fitness_values = selection.fitness_values
-            best_fitness, best_solution = self.get_best_solution(fitness_values)
-            print(
-                f'Generation {generation + 1} best individual: {best_solution}, best_distance: {1 / best_fitness}')
+            # best_fitness, best_solution = self.get_best_solution(fitness_values)
 
-            # 엘리트 선택 elites = population_size * elite_size
+            # 결과 출력
+            # best_distance = int(1 / best_fitness)
+            # if prev_result != best_distance:
+            #     prev_result = best_distance
+            #     print(f'Generation {generation + 1} best individual: {best_solution}, best_distance: {best_distance}')
+
+            # 엘리트 선택
             elite_size = int(self.population_size * self.elitism_rate)
             elites = [self.population[i] for i in np.argsort(fitness_values)[::-1][:elite_size]]
 
@@ -68,41 +73,16 @@ class ScheduleGA:
         best_solution_idx = self.population[best_idx]
         best_solution = [self.block_dict[idx] for idx in best_solution_idx]
 
-        return best_fitness, best_solution_idx
+        return best_fitness, best_solution
 
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-
-    def plot_multiple_list_data(data_list, color_list=None):
-        if not color_list:
-            color_list = ['blue', 'red', 'green']  # 기본값 설정
-
-        x = range(len(data_list[0]))  # x축 범위는 첫 번째 리스트 데이터의 길이로 설정
-
-        for i, data in enumerate(data_list):
-            y = data
-            color = color_list[i % len(color_list)]  # color_list를 순환하도록 설정
-
-            plt.plot(x, y, color=color)
-
-        plt.show()
-
 
     blocks = []
     for i in range(15):
-        block = Block(i+1, 1, 1, 1, 1, 1, [0, i], [0, i+1])
+        block = Block(i + 1, 1, 1, 1, 1, 1, [0, i], [0, i + 1])
         blocks.append(block)
 
-    ga = ScheduleGA(blocks, population_size=30, max_generation=300)
-    data_list = []
-    for i in range(3):
-
-        population = ga.init_population()
-        block_dict = ga.block_dict_init()
-        selection = Selection(population, block_dict)
-        data_list.append(selection.get_cumulative_prob())
-
-    colors = ['blue', 'red', 'green']
-    plot_multiple_list_data(data_list, colors)
-
+    ga = ScheduleGA(blocks, population_size=60, max_generation=1000)
+    sol = ga.run()
+    print(sol)
